034cdde0b5031e23846e03b23c7daff877d3c89a
SaikumarG18 <157361067+SaikumarG18@users.noreply.github.com>
2024-01-23 22:01:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import snowflake.connector
+
 streamlit.title('my parents new healthy diner')
 streamlit.header('breakfast menu')
 streamlit.text(' 🥣 omega 3 & blueberry oatmeal')
@@ -29,6 +29,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # loading fruityvice_normalized in to table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
 streamlit.header("View Our Fruit List - Add Your Favorites!") 
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
